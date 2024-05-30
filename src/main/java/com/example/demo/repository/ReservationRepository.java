09ff050958d9909cefa126ffd93fcdfeda982fec
thisIsCodyLin1201 <120441482+thisIsCodyLin1201@users.noreply.github.com>
2024-05-30 17:08:40 +0800

    Update

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -2,21 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    // 根据用户ID查找预约
-    List<Reservation> findByUser_UserId(Long userId);
-
-    // 根据锁定器ID查找预约
-    List<Reservation> findByLocker_LockerId(Long lockerId);
-
-    // 根据酒店ID查找预约
-    List<Reservation> findByHotel_HotelId(Long hotelId);
-
-    // 根据交付ID查找预约
-    Optional<Reservation> findByDeliveryID(Long deliveryId);
+    @Query(value="SELECT * FROM reservation AS r WHERE r.userId = %?1", nativeQuery = true)
+    Optional<Reservation> findByUser_ID(Long userID);
+    @Query(value="SELECT * FROM reservation AS r WHERE r.reservationId = %?1", nativeQuery = true)
+    Optional<Reservation> findByReservation_Id(Long reservationID);
 }
